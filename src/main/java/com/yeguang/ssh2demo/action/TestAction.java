5ea1c97f2539f2d249698cd95f30d3292b354c76
zhang13730865557@163.com <zhang13730865557@163.com>
2015-06-10 23:36:00 +0800

    spring+struct+log4j

---
+++ src/main/java/com/yeguang/ssh2demo/action/TestAction.java
@@ -0,0 +1,37 @@
+package com.yeguang.ssh2demo.action;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+
+
+@ParentPackage("testPackage")
+public class TestAction {
+    
+	private String result;  
+    private String type; 	 
+	
+	@Action(value = "strust2Test", results = {@Result(name="success", type = "json")})
+	public String test() {
+		System.out.println("进入TestAction");
+		setType("json");
+		setResult("ok");
+		return "success";
+	}
+	
+	public String getResult() {  
+        return result;  
+    }  
+  
+    public void setResult(String result) {  
+        this.result = result;  
+    }  
+  
+    public String getType() {  
+        return type;  
+    }  
+  
+    public void setType(String type) {  
+        this.type = type;  
+    }  
+}
